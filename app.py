8a7a199359362df621a9647d71606b0d503123b1
Titouan Cocheril <titouan.cocheril25@gmail.com>
2024-06-25 09:29:29 +0200

    @dev: send file to server working

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
-from ftplib import FTP
+import paramiko
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
@@ -10,21 +13,29 @@ UPLOAD_FOLDER = 'uploads'
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
-FTP_HOST = ""
-FTP_USER = ""
-FTP_PASS = ""
+SSH_HOST = os.getenv('SSH_HOST')
+SSH_PORT = int(os.getenv('SSH_PORT', 22))
+SSH_USER = os.getenv('SSH_USER')
+SSH_PASS = os.getenv('SSH_PASS')
+REMOTE_PATH = os.getenv('REMOTE_PATH')
 
-def upload_to_ftp(local_filepath, remote_filename):
+
+def upload_to_ssh(local_filepath, remote_filepath):
     try:
-        ftp = FTP(FTP_HOST)
-        ftp.login(FTP_USER, FTP_PASS)
-        with open(local_filepath, 'rb') as file:
-            ftp.storbinary(f'STOR {remote_filename}', file)
-        ftp.quit()
-        print(f'Successfully uploaded {local_filepath} to {remote_filename}')
+        transport = paramiko.Transport((SSH_HOST, SSH_PORT))
+        transport.connect(username=SSH_USER, password=SSH_PASS)
+
+        sftp = paramiko.SFTPClient.from_transport(transport)
+        sftp.put(local_filepath, remote_filepath)
+
+        sftp.close()
+        transport.close()
+
+        print(f'Successfully uploaded {local_filepath} to {remote_filepath}')
     except Exception as e:
-        print(f'Error uploading {local_filepath} to FTP: {e}')
+        print(f'Error uploading {local_filepath} via SSH: {e}')
         raise
+
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -40,12 +51,15 @@ def upload_file():
         file.save(local_filepath)
         print(f'File saved to {local_filepath}')
 
-        upload_to_ftp(local_filepath, file.filename)
+        remote_filepath = os.path.join(REMOTE_PATH, file.filename)
+
+        upload_to_ssh(local_filepath, remote_filepath)
 
         return jsonify({"message": "File successfully uploaded and transferred"}), 200
     except Exception as e:
         print(f'Internal Server Error: {e}')
         return jsonify({"error": "Internal Server Error"}), 500
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
